3d84624e81ac6c2b6c2af823d578c9ddced1e06d
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-09-16 19:42:16 +0530

    Update login.component.ts

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
